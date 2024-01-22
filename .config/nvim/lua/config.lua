b4b88d30e9f15018f88f5330d6f48bbaf9bc690d
Ryan Dalby <ryadalby@amazon.com>
2024-01-22 16:11:14 -0700

    Update nvim autocomplete keybinds

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -135,7 +135,7 @@ cmp.setup({
   --   end, {"i","s","c",}),
   -- })
   mapping = {
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<TAB>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -150,7 +150,7 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ["<S-TAB>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
