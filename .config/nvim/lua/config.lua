5ff488c8288e3ceff99358fcb457e3f28226dac3
Ryan Dalby <ryadalby@amazon.com>
2024-08-30 23:49:58 +0000

    Simplify nvim config

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -63,9 +63,6 @@ require('lazy').setup({
   -- Autocompletion
   {
     'hrsh7th/nvim-cmp',
-    dependencies = {
-      {'L3MON4D3/LuaSnip'},
-    },
   },
   {
     'hrsh7th/cmp-buffer',
@@ -79,6 +76,9 @@ require('lazy').setup({
     event = "InsertEnter",
     config = true
   },
+  -- vsnip
+  {'hrsh7th/cmp-vsnip'},
+  {'hrsh7th/vim-vsnip'},
 })
 
 ---
@@ -127,69 +127,38 @@ require('mason-lspconfig').setup({
 -- Autocompletion config
 ---
 local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
-
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local luasnip = require("luasnip")
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     end,
   },
 -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
   mapping = cmp.mapping.preset.insert({
-    ["<TAB>"] = cmp.mapping(function(fallback)
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
       if cmp.visible() then
-        cmp.select_next_item()
-      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-      -- that way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        cmp.mapping.complete()
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-TAB>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+        local entry = cmp.get_selected_entry()
+	if not entry then
+	  cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+	else
+	  cmp.confirm()
+	end
       else
         fallback()
       end
-    end, { "i", "s" }),
-    -- ["<Tab>"] = cmp.mapping(function(fallback)
-    --   -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-    --   if cmp.visible() then
-    --     local entry = cmp.get_selected_entry()
-	-- if not entry then
-	  -- cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-	-- else
-	  -- cmp.confirm()
-	-- end
-    --   else
-    --     fallback()
-    --   end
-    -- end, {"i","s","c",}),
+    end, {"i","s","c",}),
 
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-c>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = 'vsnip' },
   }, {
     {
       name = 'buffer',
